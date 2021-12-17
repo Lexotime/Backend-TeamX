cc6b5567ae98ce5f6e077c18cf38bbe33e396a29
Lexotime <alexandrendene4@gmail.com>
2021-12-17 18:39:17 +0000

    final original

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,23 +6,28 @@ export class ProductController{
     constructor(private productService: ProductService){}
 
     @Get()
-    findAllProduct(){
-
+    async findAllProduct(){
+        const prod = await this.productService.recupAllProduct();
+        return prod;
     }
     @Get(':id')
-    findOneProduct(){
-
+    async findOneProduct(@Param('id') id: string){
+        const prod = await this.productService.recupOneProduct(id);
+        return prod;
     }
     @Post()
-    addProduct(){
-
+    async addProduct(@Body('name') name: string,@Body('price') price: string, @Body('quantity') quantity: string, @Body('image') image: string){
+        const result = await this.productService.ajouterProduct(name,price,quantity,image);
+        return result;
     }
     @Patch(':id')
-    updateProduct(){
-
+    async updateProduct(@Param('id') id: string, @Body('name') name: string, @Body('price') price: string, @Body('quantity') quantity: string, @Body('image') image: string){
+        const result = await this.productService.modifierProduct(id,name,price,quantity,image);
+        return result;
     }
     @Delete(':id')
-    deleteProduct(){
-        
+    async deleteProduct(@Param('id') id: string){
+        const result = await this.productService.supprimerProduit(id);
+        return result;
     }
 }
